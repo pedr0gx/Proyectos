8f940dd5b1c0e19dfa4168c64231defbc98f4f23
Pedro Gonzalez <67808114+pedr0gx@users.noreply.github.com>
2023-05-12 12:23:41 -0500

    Change extensions

---
+++ Bat/Cambiar Extensiones/ChangeExt.bat
@@ -0,0 +1,27 @@
+@echo off
+
+title Change Extensions
+MODE CON COLS=40 LINES=18
+:volver
+cls
+echo.
+echo.
+echo      ***************************
+echo      *                         *
+echo      *    Change Extensions    *
+echo      * For Pedro Gonzalez 2023 *  
+echo      *                         *
+echo      ***************************
+echo.
+
+set/p ext1= Ext Current: 
+set/p ext2= Ext New: 
+
+ren *.%ext1% *.%ext2%
+
+echo.
+echo.
+echo  -- extensions changed successfully --
+echo    --- Enter for for new change ---
+pause>nul
+goto volver
